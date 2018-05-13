0bf03083cf392eb26fc105b005c5938c7860e5eb
Olivier <olivier@veilleperso.com>
2018-05-13 15:21:51 +0200

    Fundings

--- lib/huobi/rest/private/accounts.rb
+++ lib/huobi/rest/private/accounts.rb
@@ -17,13 +17,37 @@ module Huobi
 
           params.delete_if { |key, value| value.nil? }
 
-          response  =   get("/v1/account/accounts", params: params, options: options)#&.fetch("data", [])
-          #::Huobi::Models::OHLCV.parse(response) if response
+          get("/v1/account/accounts", params: params, options: options)#&.fetch("data", [])
         end
 
         def balance(account_id: nil)
           options = { authenticate: true }
           get("/v1/hadax/account/accounts/#{account_id}/balance", options: options)&.fetch("data", [])
+        end
+
+        def deposits(currency, options = {})
+          options[:authenticate] = true
+          params = {
+            currency: currency.to_s.downcase,
+            type: 'deposit',
+            from: options.delete(:from) || 1,
+            size: options.delete(:size) || 50
+          }
+          params.delete_if { |key, value| value.nil? }
+          get("/v1/query/deposit-withdraw", params: params, options: options)&.fetch("data", [])
+        end
+
+        def withdraws(currency, options = {})
+          options[:authenticate] = true
+          params = {
+            currency: currency.to_s.downcase,
+            type: 'withdraw',
+            from: options.delete(:from) || 1,
+            size: options.delete(:size) || 50
+          }
+          params.delete_if { |key, value| value.nil? }
+
+          get("/v1/query/deposit-withdraw", params: params, options: options)&.fetch("data", [])
         end
       end
     end
--- lib/huobi/rest/public/markets.rb
+++ lib/huobi/rest/public/markets.rb
@@ -2,29 +2,29 @@ module Huobi
   module Rest
     module Public
       module Markets
-        
+
         def symbols(options: {})
-          response    =   get("/v1/common/symbols", options: options)&.fetch("data", [])
+          response    =   get("/v1/hadax/common/symbols", options: options)&.fetch("data", [])
           ::Huobi::Models::Symbol.parse(response) if response
         end
-        
+
         def currencies(options: {})
-          response    =   get("/v1/common/currencys", options: options)&.fetch("data", [])
+          response    =   get("/v1/hadax/common/currencys", options: options)&.fetch("data", [])
         end
-        
+
         def ticker(symbol, options: {})
           symbol      =   ::Huobi::Utilities::Parameters.fix_symbol(symbol)
           response    =   get("/market/detail/merged", params: {symbol: symbol}, options: options)&.fetch("tick", {})
           ::Huobi::Models::Ticker.new(response) if response
         end
-        
+
         def market_detail(symbol, options: {})
           symbol      =   ::Huobi::Utilities::Parameters.fix_symbol(symbol)
           response    =   get("/market/detail", params: {symbol: symbol}, options: options)
           response    =   response&.fetch("tick", {})&.merge("ts" => response&.fetch("ts", nil))
           ::Huobi::Models::OHLCV.new(response) if response
         end
-      
+
       end
     end
   end
