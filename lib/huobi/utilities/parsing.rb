aa18e1f8feda8a1b8b96c0f8f7e0319fe9efdf27
Olivier <olivier@veilleperso.com>
2019-03-04 10:49:10 +0100

    Fix bigdecimal warning

--- lib/huobi/utilities/parsing.rb
+++ lib/huobi/utilities/parsing.rb
@@ -1,60 +1,60 @@
 module Huobi
   module Utilities
     class Parsing
-      
+
       class << self
         def convert_value(value, type, use_ms_for_time: true)
           if type.is_a?(Symbol)
             return case type
-              when :string
-                value.to_s
-              when :symbol
-                value.to_s.underscore.downcase.to_sym
-              when :integer
-                value.to_i
-              when :float
-                value.to_f
-              when :decimal
-                to_decimal(value)
-              when :boolean
-                ["true", "1"].include?(value.to_s.downcase)
-              when :datetime
-                DateTime.parse(value)
-              when :time
-                epoch_to_time(value, ms: use_ms_for_time)
-              when :hash
-                value.symbolize_keys
-              else
-                value
+            when :string
+              value.to_s
+            when :symbol
+              value.to_s.underscore.downcase.to_sym
+            when :integer
+              value.to_i
+            when :float
+              value.to_f
+            when :decimal
+              to_decimal(value)
+            when :boolean
+              ["true", "1"].include?(value.to_s.downcase)
+            when :datetime
+              DateTime.parse(value)
+            when :time
+              epoch_to_time(value, ms: use_ms_for_time)
+            when :hash
+              value.symbolize_keys
+            else
+              value
             end
           elsif type.is_a?(Hash)
             return case type.keys.first
-              when :enum
-                type[:enum][value.to_i]
-              else
-                value
+            when :enum
+              type[:enum][value.to_i]
+            else
+              value
             end
           end
         end
-      
+
         def time_to_epoch(time, ms: false)
           ms ? (time.to_i * 1_000) : time.to_i
         end
-      
+
         def epoch_to_time(epoch, ms: false)
           ms ? ::Time.at(epoch.to_i / 1_000).utc : ::Time.at(epoch.to_i).utc
         end
-      
+
         def numeric?(string)
-          !!Kernel.Float(string) 
+          !!Kernel.Float(string)
         rescue TypeError, ArgumentError
           false
         end
-      
+
         def to_decimal(number)
           if number
             num = number.to_s
-            num.empty? ? nil : BigDecimal.new(num)
+            num.empty? ? nil : BigDecimal(num)
           else
             nil
           end
@@ -64,9 +64,9 @@ module Huobi
           result = number / divisor
           result.nan? || result.infinite? ? 0 : result
         end
-          
+
       end
-      
-    end    
+
+    end
   end
 end
